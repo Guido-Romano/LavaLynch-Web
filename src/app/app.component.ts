14b2ccf302ce1c51b45456fb1eaea5dd591c22ca
apocca <guidoromano1989@gmail.com>
2024-05-13 23:08:20 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('pElement') pElement!: ElementRef;
+
   title = 'lavalynch-2.0';
+
+  ngOnInit() {
+    AOS.init()
+    window.addEventListener('load',AOS.refresh)
+  }
 }
